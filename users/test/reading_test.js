c3433d8355478c03ce3b695ff0efb61db9187d81
oliverlai55 <oliverlai55@gmail.com>
2017-02-10 00:31:41 -0500

    Add Reading test file

---
+++ users/test/reading_test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Reading users out of the database', () => {
+  let joe;
+  
+  beforeEach(() => {
+    //we don't use var because we want joe to be global so inside it, can read it as well
+    joe = new User({ name: 'Joe '});
+    joe.save()
+      .then(() => done());
+  });
+
+  it('finds all users with a name of Joe', () => {
+
+  });
+});

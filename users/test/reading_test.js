b67f81c00491879f4a8978b68cf195b8cee863a6
oliverlai55 <oliverlai55@gmail.com>
2017-02-10 00:47:54 -0500

    Add code to grab data from database

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -3,15 +3,19 @@ const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
   let joe;
-  
-  beforeEach(() => {
+
+  beforeEach((done) => {
     //we don't use var because we want joe to be global so inside it, can read it as well
-    joe = new User({ name: 'Joe '});
+    joe = new User({ name: 'Joe' });
     joe.save()
       .then(() => done());
   });
 
-  it('finds all users with a name of Joe', () => {
-
+  it('finds all users with a name of Joe', (done) => {
+    User.find({ name: 'Joe' })
+      .then((users) => {
+        console.log(users);
+        done();
+      });
   });
 });

3360ee9b995af6ddf595db4f7c78fabc45ddd3bc
oliverlai55 <oliverlai55@gmail.com>
2017-02-10 00:31:53 -0500

    Set up test helper file

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  //wait for mongo to 'open' then do something
-  .on('error', (error) => {
-    // wait for mongoose to see if there is an 'error' then do something
-    console.warn('Warning', error);
+mongoose.Promise = global.Promise;
+// global.promise references to the ES6 Implementation of Promise
+
+before((done) => {
+//Only executed one time in the entire test suite
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once('open', () => { done(); })
+    //wait for mongo to 'open' then do something
+    .on('error', (error) => {
+      // wait for mongoose to see if there is an 'error' then do something
+      console.warn('Warning', error);
+    });
+});
+
+
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next test
+    done();
+    // this tells mocha that we are done, and can proceed with mocha
   });
+});

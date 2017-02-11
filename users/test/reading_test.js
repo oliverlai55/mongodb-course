2829d9e7a953b83be54144427409c607b3927cce
oliverlai55 <oliverlai55@gmail.com>
2017-02-11 02:46:16 -0500

    Compare user string id

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -14,7 +14,7 @@ describe('Reading users out of the database', () => {
   it('finds all users with a name of Joe', (done) => {
     User.find({ name: 'Joe' })
       .then((users) => {
-        console.log(users);
+        assert(users[0]._id.toString() === joe._id.toString());
         done();
       });
   });

0a855dcbec0275f941d78d338b24992436193cd3
oliverlai55 <oliverlai55@gmail.com>
2017-02-18 00:33:41 -0500

    Add real case validation

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -17,4 +17,16 @@ describe('Validating records', () => {
 
     assert(message === 'Name must be longer than 2 characters.');
   });
+
+//This is how you usually see it being used.
+  it('disallows invalid records from being saved', (done) => {
+    const user = new User({ name: 'Al' });
+    user.save()
+      .catch((validationResult) => {
+        const { message } = validationResult.errors.name;
+
+        assert(message === 'Name must be longer than 2 characters.');
+        done();
+      });
+  });
 });

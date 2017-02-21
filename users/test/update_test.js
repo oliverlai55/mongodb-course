cd7b1013254c34e621cdef2e9c164fb132ca3625
oliverlai55 <oliverlai55@gmail.com>
2017-02-21 00:29:45 -0500

    Update test to likes

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0 });
+    joe = new User({ name: 'Joe', likes: 0 });
     joe.save()
       .then(() => done());
   });
@@ -53,12 +53,12 @@ describe('Updating records', () => {
   });
 
 // Adding x meaning mocha will not execute test
-  xit('A user can have their post count incremented by 1', () => {
+  it('A user can have their post count incremented by 1', () => {
     // $inc will increment value by whatever you set
-    User.update({ name: 'Joe' }, { $inc: { postcount: 1 } })
+    User.update({ name: 'Joe' }, { $inc: { likes: 10 } })
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {
-        assert(user.postCount === 1);
+        assert(user.likes === 10);
         done();
       });
   });

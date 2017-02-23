5ba2713f1b150eb1005f211f3be6c2d5cc5ca5f2
oliverlai55 <oliverlai55@gmail.com>
2017-02-23 00:03:06 -0500

    Add new model

--- UpStarMusic/database/models/artist.js
+++ UpStarMusic/database/models/artist.js
@@ -1,2 +1,18 @@
 // Todo: Create Artist Model
 //Artists has subdocument of album
+const mongoose = require('mongoose');
+const AlbumSchema = require('./album');
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema]
+});

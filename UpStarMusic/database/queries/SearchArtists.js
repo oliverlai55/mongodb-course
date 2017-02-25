2d2e0465e79c029ba26757273766b8f6d2dad77a
oliverlai55 <oliverlai55@gmail.com>
2017-02-25 14:52:30 -0500

    Add query

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,4 +9,31 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  //write query that will follow, sort, offset, limit options only
+
+// ES5
+  // const sortOrder = {};
+  // sortOrder[sortProperty] = 1;
+  //
+  // Artist.find({})
+  //   .sort({ sortOrder })
+
+// ES6 Interpolated Key
+// At run time, look at whatever sortProperty is, and give it a value of 1
+  const query = Artist.find({})
+    .sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Artist.count()])
+    .then((results) => {
+      return {
+        all: results[0],
+        count: results[1],
+        offset: offset,
+        limit: limit
+      };
+    });
 };
+
+// results[0] is query, and [1] is ARtist.count()

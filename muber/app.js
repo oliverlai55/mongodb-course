715c92c5220f8b5133c8941d7664f8e1e17184cf
oliverlai55 <oliverlai55@gmail.com>
2017-03-02 23:07:49 -0500

    Set up listener

--- muber/app.js
+++ muber/app.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// Watch for incoming requests of method GET
+// to the route http://localhost:3050/api
+app.get('/api', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
 module.exports = app;

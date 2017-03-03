4ac1c6c4be37ad4851b9af2576191d0efa5614db
oliverlai55 <oliverlai55@gmail.com>
2017-03-03 00:42:05 -0500

    Refactor code

--- muber/app.js
+++ muber/app.js
@@ -1,11 +1,7 @@
 const express = require('express');
-
+const routes = require('./routes/routes');
 const app = express();
 
-// Watch for incoming requests of method GET
-// to the route http://localhost:3050/api
-app.get('/api', (req, res) => {
-  res.send({ hi: 'there' });
-});
+routes(app);
 
 module.exports = app;

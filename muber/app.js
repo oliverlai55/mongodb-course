388170ad551c4bbdc117cd76fe6aec376e31266a
oliverlai55 <oliverlai55@gmail.com>
2017-03-06 02:17:38 -0500

    Update app.js

--- muber/app.js
+++ muber/app.js
@@ -13,4 +13,12 @@ if (process.env.NODE_ENV !== 'test') {
 app.use(bodyParser.json());
 routes(app);
 
+app.use((err, req, res, next) => {
+// err: populated if the previous middleware throw error
+// req: incoming request object
+// res: outgoing response object
+// next: A function to force it to go next middleware
+  res.status(422).send({ error: err.message });
+});
+
 module.exports = app;

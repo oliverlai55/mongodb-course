b6882128ebca9ede18512cdb946f978dfdad0472
oliverlai55 <oliverlai55@gmail.com>
2017-03-06 19:44:45 -0500

    Update routes

--- muber/routes/routes.js
+++ muber/routes/routes.js
@@ -8,4 +8,5 @@ module.exports = (app) => {
   app.get('/api', DriversController.greeting);
 
   app.post('/api/drivers', DriversController.create);
+  app.put('/api/drivers/:id', DriversController.edit);
 };

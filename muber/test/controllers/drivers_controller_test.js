4f6267d9a6d45dab2fbb8a9e891399f7ad914fe4
oliverlai55 <oliverlai55@gmail.com>
2017-03-10 00:44:27 -0500

    Add test case

--- muber/test/controllers/drivers_controller_test.js
+++ muber/test/controllers/drivers_controller_test.js
@@ -32,7 +32,23 @@ describe('Drivers controller', () => {
             .then(driver => {
               assert(driver.driving === true);
               done();
-            });  
+            });
+        });
+    });
+  });
+
+  it('DELETE to /api/drivers/id can delete a driver', done => {
+    const driver = new Driver({ email: 'test@test.com' });
+
+    driver.save().then(() => {
+      request(app)
+        .delete(`/api/drivers/${driver._id}`)
+        .end(() => {
+          Driver.findOne({ email: 'test@test.com' })
+            .then((driver) => {
+              assert(driver === null);
+              done();
+            });
         });
     });
   });
